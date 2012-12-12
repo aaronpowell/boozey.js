3bae66a64ddff6f8b2c4026641d5e97824428212
Aaron Powell <me@aaron-powell.com>
2012-12-12 18:10:54 +1100

    Expanding out the cli

--- src/boozey.js
+++ src/boozey.js
@@ -4,11 +4,27 @@ var request = require('request');
 
 var baseUri = 'http://api.openbeerdatabase.com/v1/';
 
-module.exports.beers = function (key, fn) {
-    return request({
-        url: baseUri + 'beers.json',
-        qs: {
-            token: key
-        }
-    }, fn);
-}
+module.exports.version = '0.1.0';
+
+module.exports.beers = {
+    all: function (options) {
+        return request({
+            url: baseUri + 'beers.json',
+            qs: {
+                token: options.key,
+                order: (options.order || 'id') + ' ' + (options.dir || 'ASC'),
+                page: options.page || 1,
+                per_page: options.perPage || 50,
+                query: options.query
+            }
+        });
+    },
+    get: function (key, id) {
+        return request({
+            url: baseUri + 'beers/' + id + '.json',
+            qs: {
+                token: key
+            }
+        });
+    }
+};
